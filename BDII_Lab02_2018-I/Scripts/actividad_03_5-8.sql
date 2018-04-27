f05b9f540e93c39f7dba245369d03ea17429a06f
jhony <AEPIS22@AEPIS22>
2018-04-26 22:46:14 -0500

    terminado 5 y 6

--- BDII_Lab02_2018-I/Scripts/actividad_03_5-8.sql
+++ BDII_Lab02_2018-I/Scripts/actividad_03_5-8.sql
@@ -13,31 +13,13 @@ create sequence SEQ_Departamentos_ID
 
   ------6-----------------------
   
-  -- seleccionamos la tabla
+  declare @liCodigo int
+	select @liCodigo = next value for SEQ_Departamentos_ID;
+	insert into departments  values(@liCodigo,'matematica','300','3300');
+	select * from departments
 
-  
-  
-  select * from regions ;
-  select * from countries ;
-  select * from locations ;
-select * from departments;
-  -- agregamos dos registros para ver las secuencias
-
-  
+--------7-------------------------------------------------------
 
 
-  insert into regions  values('5','america');
-
-  insert into countries  values('Am','america','5');
-
-   insert into locations  values('3300','2014 america city','26588','begin','texas','Am');
-
-	insert into departments  values('300','educacion','300','3300');
-
-
-
-	-------seleccionamos la secuencia
-	
-	select next value for SEQ_Departamentos_ID;
 
 
