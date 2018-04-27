6d4a1fd585ff3cddab5f823d4803e39fd269ff2a
AEPIS14 <AEPIS14@AEPIS14>
2018-04-26 22:38:51 -0500

    Primera parte de Actividad 1

--- BDII_Lab02_2018-I/Scripts/Actividad01.sql
+++ BDII_Lab02_2018-I/Scripts/Actividad01.sql
@@ -1,1 +1,75 @@
-﻿
+﻿--4.1 Actividad N° 01: Manipulación de Datos--
+
+--RENZO MORENO
+
+--Ejercicio 1
+use [Human_Resources]
+go
+
+
+--Ejercicio 2
+create table mis_empleados (
+	empleados_id	int not null,
+	apellidos		varchar(25),
+	nombres			varchar(25),
+	codigo			varchar(10),
+	salario			decimal(9,2)
+)
+go
+
+
+--Ejercicio 3
+insert into mis_empleados values
+	(1, 'Vargas Canseco','Raúl', 'rvargas', 895),
+	(2, 'Castro Feria',  'María','mcastro', 860);
+go
+
+
+--Ejercicio 4
+create proc insertar_datos @id int, @ape varchar(25), @nom varchar(25), @cod varchar(10), @sal decimal(9,2)
+as
+insert into mis_empleados values (@id,@ape,@nom,@cod,@sal);
+go
+
+
+--Ejercicio 5
+exec insertar_datos 3,'Gómez Albán','Juan Pablo','jgomez',1100;
+exec insertar_datos 4,'Quiroz Ardiles','Judith','jquiroz',750;
+exec insertar_datos 5,'Soria Peralta','Pedro','psoria',1550;
+go
+
+
+--Ejercicio 6
+select * from mis_empleados
+go
+
+
+--Ejercicio 7
+update mis_empleados set nombres='Benjamin' where empleados_id=3;
+go
+
+
+--Ejercicio 8
+update mis_empleados set salario=1000 where salario<1000;
+go
+
+
+--Ejercicio 9
+delete from mis_empleados where codigo='mcastro';
+go
+
+
+--Ejercicio 10
+select [Begin Time],[RowLog Contents 1],[Transaction Name],Operation 
+from sys.fn_dblog(NULL,NULL) 
+where AllocUnitName='dbo.mis_empleados' and Operation IN ('LOP_DELETE_ROWS')
+go
+
+
+--Ejercicio 11
+select * from mis_empleados
+go
+
+
+--********************************--
+--ZAVALA --12 a 22
