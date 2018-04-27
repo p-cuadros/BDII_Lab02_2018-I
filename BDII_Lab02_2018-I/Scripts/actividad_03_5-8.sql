b7c183988dc28ac7333e3d0078a114c41fff583a
jhony <AEPIS22@AEPIS22>
2018-04-26 22:35:50 -0500

    6

--- BDII_Lab02_2018-I/Scripts/actividad_03_5-8.sql
+++ BDII_Lab02_2018-I/Scripts/actividad_03_5-8.sql
@@ -10,3 +10,28 @@ create sequence SEQ_Departamentos_ID
   increment by 10
   maxvalue 1000
   minvalue 200;
+
+  ------6-----------------------
+  
+  -- seleccionamos la tabla
+
+  
+  
+  select * from regions ;
+  select * from countries ;
+  select * from locations ;
+select * from departments;
+  -- agregamos dos registros para ver las secuencias
+
+  
+
+
+  insert into regions  values('5','america');
+
+  insert into countries  values('Am','america','5');
+
+   insert into locations  values('3300','2014 america city','26588','begin','texas','Am');
+
+	insert into departments  values('300','educacion','300','3300');
+
+
