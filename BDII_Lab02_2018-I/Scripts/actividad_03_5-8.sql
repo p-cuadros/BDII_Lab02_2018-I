8577af07e1526a19437d6f8bdc1a53b88cc8ba94
jhony <AEPIS22@AEPIS22>
2018-04-26 22:08:51 -0500

    actividad 3.5

--- BDII_Lab02_2018-I/Scripts/actividad_03_5-8.sql
+++ BDII_Lab02_2018-I/Scripts/actividad_03_5-8.sql
@@ -1,2 +1,12 @@
 ﻿
+ ------ 5--------------
  
+select * from departments;
+--- drop la eliminara si existe esta secuencia
+drop sequence SEQ_Departamentos_ID;
+--- creamos la secuencia
+create sequence SEQ_Departamentos_ID
+  start with 200
+  increment by 10
+  maxvalue 1000
+  minvalue 200;
