bf5ead3ac4eccc2b6fb30fc67862f9348bfa99b3
ronordonezq <37679541+ronordonezq@users.noreply.github.com>
2018-04-28 02:09:06 -0500

    Actividad 3 - ejercicio 3 y 4, por motivos de inacistencia y que los ejercicios ya estaban todos repartidos , mi compañero Hernan me compartio la mitad de su parte.

--- BDII_Lab02_2018-I/Scripts/Actividad03.sql
+++ BDII_Lab02_2018-I/Scripts/Actividad03.sql
@@ -1,1 +1,26 @@
 ﻿
+
+
+
+
+
+
+---------Ejercicio 3: creacion de vista--------------Ronald Ordoñez------------------------------
+CREATE VIEW VW_Dept50 AS
+SELECT employee_id EmpNo, last_name employee, department_id DeptNo
+FROM employees
+WHERE department_id = 50;
+GO
+--visualizamos que en la nueva vista conteniendo empleados con el department_id 50--
+select * from VW_Dept50
+
+
+---------Ejercicio 4: Actualizacion de datos del department_id de 50 a 80------
+UPDATE VW_Dept50
+SET DeptNo = 80;
+GO
+--Ya no visualizaremos datos de la vista--
+select * from VW_Dept50
+--Visualizaremos los department_id Actualizados--
+select * from employees
+-------------------------------------------------------------------------------------------------
